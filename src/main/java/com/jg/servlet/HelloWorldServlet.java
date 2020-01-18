52d49a93792926e44c71984e61749b1bea5d7394
Jacek Grzegorczyk <jacekgrzegorczyk@op.pl>
2020-01-18 13:07:59 +0100

    Adds annotation configuration for HelloWorldServlet

--- src/main/java/com/jg/servlet/HelloWorldServlet.java
+++ src/main/java/com/jg/servlet/HelloWorldServlet.java
@@ -1,11 +1,13 @@
 package com.jg.servlet;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello", "/Hello"})
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
