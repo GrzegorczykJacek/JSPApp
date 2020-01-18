1634e8b3391cd45d318817df9eb0519c6dab23f9
Jacek Grzegorczyk <jacekgrzegorczyk@op.pl>
2020-01-18 14:09:52 +0100

    Adds filter with login parameter MyFilter

--- src/main/java/com/jg/servlet/HelloWorldServlet.java
+++ src/main/java/com/jg/servlet/HelloWorldServlet.java
@@ -1,5 +1,7 @@
 package com.jg.servlet;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,13 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello", "/Hello"})
+@WebServlet(
+        name = "HelloWorldServlet"
+        , urlPatterns = {"/"}
+        , initParams = {
+                @WebInitParam(name = "userID", value = "1234567")
+                , @WebInitParam(name = "localization", value = "Poland")
+        })
 public class HelloWorldServlet extends HttpServlet {
+
+    private String varUserID, varLocalization;
+
+    @Override
+    public void init(ServletConfig config){
+        varUserID = config.getInitParameter("userID");
+        varLocalization = config.getInitParameter("localization");
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("Hello World!");
+        writer.println("User ID: " + varUserID + "<br/>");
+        writer.println(("Localization: " + varLocalization));
     }
 
 }
